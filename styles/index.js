7b38644d10854ffbc255ee329094c9d2479d3a09
jitender_verma <139689987+jit7445@users.noreply.github.com>
2024-01-22 09:54:59 +0530

    my porfolio

---
+++ styles/index.js
@@ -0,0 +1,36 @@
+document.getElementById("menu").addEventListener('click', function () {
+    document.getElementById('hamburger_menu').style.right = "0%";
+    console.log("clicked");
+  });
+
+  document.getElementById("close").addEventListener("click", () => {
+    document.getElementById('hamburger_menu').style.right = "-147%";
+    console.log("cut");
+  });
+
+  const container = document.getElementById("project");
+  const showProjectButton = document.getElementById("showproject");
+
+  showProjectButton.addEventListener('click', function () {
+    if (container.style.display === 'none' || container.style.display === '') {
+      container.style.display = 'grid';
+    } else {
+      // container.style.display = 'none';
+      container.style.transition = 'all 2s ease-in';
+        container.style.opacity = '0'; 
+        setTimeout(() => {
+            container.style.display = 'none';
+            // container.style.opacity = '1'; 
+            container.style.transition = ''; 
+        }, 3000);
+    }
+  })
+  function download(url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.download = url.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  }
+
